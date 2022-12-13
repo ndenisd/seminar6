2f999d85108d4dd9a2c4f04b31b61f5ea24247ba
denis <ndenisd@yandex.ru>
2022-12-13 21:09:59 +0300

    homework

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task 41
+
+Console.Clear();
+
+Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (Convert.ToInt32(Console.ReadLine()) > 0)
+        count++;
+}
+Console.WriteLine(count);
+
